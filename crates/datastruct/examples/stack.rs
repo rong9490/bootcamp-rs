20373de9dde6580d44f37389fddb109b9ff757dd
@hejj <hejj@tsintergy.com>
2025-04-20 12:08:07 +0800

    chore: vscode setting

--- crates/datastruct/examples/stack.rs
+++ crates/datastruct/examples/stack.rs
@@ -1,22 +1,22 @@
 fn main() {
-  println!("hello world");
+    println!("hello world");
 }
 
 #[derive(Debug)]
 struct Stack<T> {
-  size: usize, // 栈大小
-  data: Vec<T>, // 栈数据
+    size: usize,  // 栈大小
+    data: Vec<T>, // 栈数据
 }
 
 // 为栈添加方法
 impl<T> Stack<T> {
-  // 实例化空栈
-  fn new() -> Self {
-    Self {
-      size: 0,
-      data: Vec::new(),
+    // 实例化空栈
+    fn new() -> Self {
+        Self {
+            size: 0,
+            data: Vec::new(),
+        }
     }
-  }
 }
 
 // cargo test stack -- --nocapture
@@ -24,12 +24,12 @@ impl<T> Stack<T> {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_stack() {
-    let struct_type_size: usize = std::mem::size_of::<Stack<i32>>();
-    println!("{}", struct_type_size);
-    assert_eq!(struct_type_size, 32);
-  }
+    #[test]
+    fn test_stack() {
+        let struct_type_size: usize = std::mem::size_of::<Stack<i32>>();
+        println!("{}", struct_type_size);
+        assert_eq!(struct_type_size, 32);
+    }
 }
